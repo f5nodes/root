008c5d72ab8edb4c5b821f5614b211a7191fbe9c
vasylcode <vasylcode@gmail.com>
2022-06-23 18:25:47 +0200

    upd root

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # root
+
+Script, that helps you to install your node in one line.  
+List of available nodes: here
+
+---
+
+## Start
+
+To start with, just run the following in your Terminal:  
+`. <(wget -qO- https://raw.githubusercontent.com/f5nodes/root/main/root.sh)`
--- root.sh
+++ root.sh
@@ -7,6 +7,7 @@ link="https://raw.githubusercontent.com/f5nodes/$1/main/setup.sh"
 
 confirm() {
     read -r -p "${1:-Continue? [y/n]} " response
+    echo -e "\e[0m"
     case "$response" in
         [yY][eE][sS]|[yY]) 
             true
@@ -21,18 +22,18 @@ confirm() {
 if [ -z "$1" ]; then
 	echo -e  "\n\e[91mERROR: Enter a node name as a first argument!\e[0m"
 	echo -e  "\e[91mERROR: Example: \e[4mbash $0 <node name> <language> (optional)\e[0m\n"
-	exit 1
+	# exit 1
 fi
 # ARG 2: if language is provided by user
 [ -n "$2" ] && language=${2,,}
 if !(echo "$languages"  | fgrep -q -w "$language"); then
 	echo -e "\e[91mLanguage is not supported, you can make a pull request with here - \e[4mgithub.com/f5nodes/$1\e[0m\n"
-	exit 1
+	# exit 1
 fi
 
 if wget -q --spider $link; then
 	echo -e "\n\e[93mYou select installing the node \e[92m${1^} \e[93mwith \e[92m${language^^} \e[93mlanguage!\e[0m"
-	confirm && wget -qO- $link | bash -s $language
+	confirm $'\e[93mContinue?\e[92m' && wget -qO- $link | bash -s $language
 else
 	echo -e  "\n\e[91mERROR: This node doesn't exist!\e[0m"
 	echo -e  "\e[91mERROR: Available nodes: \e[4mgithub.com/f5nodes\e[0m\n"
