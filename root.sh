1932fc168c7183d27386a51c23f9fd85406d8652
vasylcode <vasylcode@gmail.com>
2022-06-26 12:19:42 +0200

    update root

--- root.sh
+++ root.sh
@@ -19,23 +19,42 @@ confirm() {
 }
 
 # ARG 1: if node name is empty
-: ${1?"Usage: $0 ARGUMENT"}
+if [ -z "$1" ]; then
+    echo -e  "\n\e[91mERROR: Enter a node name as a first argument!\e[0m"
+    echo -e  "\e[91mERROR: Example: \e[4mbash $0 <node name> <language> (optional)\e[0m\n"
+    return 1 2>/dev/null; exit 1
+fi
 # if [ -z "$1" ]; then
 # 	echo -e  "\n\e[91mERROR: Enter a node name as a first argument!\e[0m"
 # 	echo -e  "\e[91mERROR: Example: \e[4mbash $0 <node name> <language> (optional)\e[0m\n"
 # 	exit 1
 # fi
 # ARG 2: if language is provided by user
-[ -n "$2" ] && language=${2,,}
-if !(echo "$languages" | grep -Fqw "$language"); then
-	echo -e "\n\e[91mLanguage is not supported, you can make a pull request with here - \e[4mgithub.com/f5nodes/$1\e[0m\n"
-	exit 1
+# [ -n "$2" ] && language=${2,,}
+if [ -n "$2" ]; then
+    language=${2,,}
+    echo -e "\n\e[91mSupported lang!\e[0m\n"
 fi
-
+if ! echo "$languages" | grep -Fqw "$language"; then
+    echo -e "\n\e[91mLanguage is not supported, you can make a pull request with here - \e[4mgithub.com/f5nodes/$1\e[0m\n"
+    return 1 2>/dev/null; exit 1
+fi
+# if !(echo "$languages" | grep -Fqw "$language"); then
+# 	echo -e "\n\e[91mLanguage is not supported, you can make a pull request with here - \e[4mgithub.com/f5nodes/$1\e[0m\n"
+# 	exit 1
+# fi
 if wget -q --spider $link; then
-	echo -e "\n\e[93mYou select installing the node \e[92m${1^} \e[93mwith \e[92m${language^^} \e[93mlanguage!\e[0m"
-	confirm $'\e[93mContinue?\e[92m' && wget -qO- $link | bash -s $language
+  echo -e "\n\e[93mYou select installing the node \e[92m${1^} \e[93mwith \e[92m${language^^} \e[93mlanguage!\e[0m"
+  confirm $'\e[93mContinue?\e[92m' && wget -qO- $link | bash -s $language
 else
-	echo -e  "\n\e[91mERROR: This node doesn't exist!\e[0m"
-	echo -e  "\e[91mERROR: Available nodes: \e[4mgithub.com/f5nodes\e[0m\n"
+  echo -e  "\n\e[91mERROR: This node doesn't exist!\e[0m"
+  echo -e  "\e[91mERROR: Available nodes: \e[4mgithub.com/f5nodes\e[0m\n"
+  return 1 2>/dev/null; exit 1
 fi
+# if wget -q --spider $link; then
+# 	echo -e "\n\e[93mYou select installing the node \e[92m${1^} \e[93mwith \e[92m${language^^} \e[93mlanguage!\e[0m"
+# 	confirm $'\e[93mContinue?\e[92m' && wget -qO- $link | bash -s $language
+# else
+# 	echo -e  "\n\e[91mERROR: This node doesn't exist!\e[0m"
+# 	echo -e  "\e[91mERROR: Available nodes: \e[4mgithub.com/f5nodes\e[0m\n"
+# fi
