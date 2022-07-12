09a59535ed4599476f5e28efee66c89442f274e3
vasylcode <vasylcode@gmail.com>
2022-07-12 11:55:00 +0200

    available update

--- available.sh
+++ available.sh
@@ -6,4 +6,5 @@ echo -e "\e[92mironfish: \e[93minstall, update, delete\e[0m"
 echo -e "\e[92mmasa: \e[93minstall\e[0m"
 echo -e "\e[92mstarknet: \e[93minstall, update\e[0m"
 echo -e "\e[92msubspace: \e[93minstall\e[0m"
+echo -e "\e[92mobol: \e[93mget enr key\e[0m"
 echo
