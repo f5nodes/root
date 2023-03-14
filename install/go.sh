1adb3c19e06413544775432722468116378a3383
vasylcode <vasylcode@gmail.com>
2023-03-14 09:00:07 +0100

    go.sh update

--- install/go.sh
+++ install/go.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
-sudo rm go1.20.2.linux-amd64.tar.gz
+ver="1.20.2"
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+sudo rm "go$ver.linux-amd64.tar.gz"
 echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
 echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
 echo 'export GO111MODULE=on' >> $HOME/.bash_profile
